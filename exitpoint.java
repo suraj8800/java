c04a8d4381be361f7f0027562dbeb6ab876be577
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-10-04 16:07:19 +0530

    exitpoint

--- exitpoint.java
+++ exitpoint.java
@@ -11,5 +11,40 @@ public class exitpoint {
                 arr[i][j] = scn.nextInt();
             }
         }
+
+        int i =0; 
+        int j = 0;
+        int dir =0; // 0 is east, 1 is south, 2 is west, 3 is north
+        while(true){
+            dir = (dir + arr[i][j]) % 4;
+
+            if(dir ==0 ){ //east
+                j++;
+                if(j == arr[0].length){
+                    j--;
+                    break;
+                }
+            } else if(dir == 1){ //south
+                i++;
+                if(i == arr.length){
+                    i--;
+                    break;
+                }
+            } else if(dir == 2){//west
+                j--;
+                if(j ==-1){
+                    j++;
+                    break;
+                }
+            } else{//north
+                i--;
+                if(i == -1){
+                    i++;
+                    break;
+                }
+            }
+        }
+        System.out.println(i);
+        System.out.println(j);
     }
 }
