650cd227648f68a7a5bd952916573367d40d92ca
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-10-05 19:55:48 +0530

    rotateby90

--- 2D Array/matrixmultiple.java
+++ 2D Array/matrixmultiple.java
@@ -1,14 +1,13 @@
 import java.util.*;
-
-public class matrixmultiple {
+public class matrixmultiple{
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
         int n1 = scn.nextInt();
         int m1 = scn.nextInt();
         int[][] one = new int[n1][m1];
-        for (int i = 0; i < one.length; i++) {
-            for (int j = 0; j < one[0].length; i++) {
+        for(int i=0; i<one.length; i++){
+            for(int j=0; j<one[0].length; j++){
                 one[i][j] = scn.nextInt();
             }
         }
@@ -16,8 +15,8 @@ public class matrixmultiple {
         int n2 = scn.nextInt();
         int m2 = scn.nextInt();
         int[][] two = new int[n2][m2];
-        for (int i = 0; i < two.length; i++) {
-            for (int j = 0; j < two[0].length; j++) {
+        for(int i =0; i<two.length; i++){
+            for(int j=0; j<two[0].length; j++){
                 two[i][j] = scn.nextInt();
             }
         }
@@ -28,10 +27,9 @@ public class matrixmultiple {
         }
 
         int[][] prd = new int[n1][m2];
-        for(int i=0; i<prd.length; i++){
+        for(int i = 0; i<prd.length; i++){
             for(int j=0; j<prd[0].length; j++){
-                int val = 0; 
-
+                int val =0;
                 for(int k=0; k<m1; k++){
                     val += one[i][k] * two[k][j];
                 }
@@ -39,9 +37,9 @@ public class matrixmultiple {
             }
         }
 
-        for(int i=0; i<prd.length; i++){
+        for(int i = 0; i<prd.length; i++){
             for(int j=0; j<prd[0].length; j++){
-                System.out.print(prd[i][j]+ " ");
+                System.out.print(prd[i][j] +" ");
             }
             System.out.println();
         }
