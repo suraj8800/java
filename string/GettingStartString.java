528cf9e6a0cac45e98f2f856201f9bd96a7d6be1
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-10-14 19:24:03 +0530

    Create GettingStartString.java

---
+++ string/GettingStartString.java
@@ -0,0 +1,13 @@
+package string;
+import java.io.*;
+import java.util.*;
+
+public class GettingStartString {
+    public static void main(String[] args) {
+        
+        String  s = "Hello";
+        System.out.println(s);
+
+        System.out.println(10 + 20 + s + 10 + 20);
+    }
+}
