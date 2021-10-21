7f85bc1a699de7b275f0ff387e1e4cd2b07224b7
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-10-21 16:36:44 +0530

    Create printzigzag.java

---
+++ recurrsion/printzigzag.java
@@ -0,0 +1,24 @@
+package recurrsion;
+import java.io.*;
+import java.util.*;
+public class printzigzag {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }
+    // E => pzz(3) = 3 211121112 3 211121112 3
+    // F => pzz(2) = 2 111 2 111 2
+    // EWF => pzz(3) = 3 pzz(2) 3 pzz(2) 3
+    public static void pzz(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.print(n + " ");
+        pzz(n-1);
+        System.out.print(n + " ");
+        pzz(n-1);
+        System.out.print(n + " ");
+    }
+}
+
