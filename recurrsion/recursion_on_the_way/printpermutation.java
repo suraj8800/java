16b0b5fafb656db305ebccd7a872b426e5b0e76b
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-10-31 19:24:23 +0530

    Create printpermutation.java

---
+++ recurrsion/recursion_on_the_way/printpermutation.java
@@ -0,0 +1,27 @@
+package recurrsion.recursion_on_the_way;
+
+import java.util.Scanner;
+
+public class printpermutation {
+    public static void main(String[] args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        printPermutations(str, "");
+    }
+
+    public static void printPermutations(String str, String asf){
+        if(str.length() ==0){
+            System.out.println(asf);
+            return;
+        }
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            String prei = str.substring(0, i);
+            String posti = str.substring(i+1);
+
+            String newstr = prei + posti;
+            printPermutations(newstr, asf+ch);
+        }
+    }
+}
