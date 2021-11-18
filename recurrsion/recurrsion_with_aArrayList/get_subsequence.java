cb3a2a3ec0e692175ef9056192972ceaf193c555
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-11-18 16:01:35 +0530

    Update get_subsequence.java

--- recurrsion/recurrsion_with_aArrayList/get_subsequence.java
+++ recurrsion/recurrsion_with_aArrayList/get_subsequence.java
@@ -4,36 +4,34 @@ import java.io.*;
 import java.util.*;
 
 public class get_subsequence {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
         ArrayList<String> ss = gss(str);
         System.out.println(ss);
     }
 
-    public static ArrayList<String> gss(String str) {
+    public static ArrayList<String> gss(String str){
         if(str.length() == 0){
             ArrayList<String> bres = new ArrayList<>();
-            String theSS = "";
-            bres.add(theSS);
+            bres.add("");
             return bres;
         }
 
-        char ch = str.charAt(0); // abc -> a
-        String ros = str.substring(1); // abc -> bc
-
-        ArrayList<String> rres = gss(ros); // ros = [.., .c, bc]
+        char ch = str.charAt(0); // x
+        String ros = str.substring(1); //yz
+        ArrayList<String> rres = gss(ros);
         ArrayList<String> mres = new ArrayList<>();
 
-        for(String rstr: rres){
-            mres.add(rstr);
+        for(String rstr:rres){
+            String mstr = rstr;
+            mres.add(mstr);
         }
 
         for(String rstr: rres){
-            mres.add(ch + rstr);
+            String mstr = ch + rstr;
+            mres.add(mstr);
         }
         return mres;
     }
-
 }
