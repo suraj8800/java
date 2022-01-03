e58c2310bc68054d8e18cf6dc5e1ddb99efaffe6
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2022-01-03 23:10:25 +0530

    Create stackdemo.java

---
+++ stack_and_queues/stackdemo.java
@@ -0,0 +1,27 @@
+package stack_and_queues;
+
+import java.util.Stack;
+
+public class stackdemo {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        System.out.println(stack.peek());
+        stack.pop();
+
+        System.out.println(stack.peek());
+        stack.pop();
+
+        System.out.println(stack.peek());
+        stack.pop();
+
+        System.out.println(stack.peek());
+        stack.pop();
+
+    }
+}
+
