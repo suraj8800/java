5be0c28894536db6d2de892753efbbf11506621a
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2022-01-23 12:58:33 +0530

    Update binary.java

--- Binary_Tree/binary.java
+++ Binary_Tree/binary.java
@@ -1,7 +1,6 @@
 package Binary_Tree;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 //import java.lang.reflect.Constructor;
 import java.util.Stack;
